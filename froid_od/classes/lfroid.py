e4b27e460bb2fb481e572bf069c5febfe05ef98c
cri98li <c.landi7@studenti.unipi.it>
2023-07-21 18:08:19 +0200

    Implementing and testing L-FROID

--- froid_od/classes/lfroid.py
+++ froid_od/classes/lfroid.py
@@ -5,6 +5,7 @@ from tqdm.auto import tqdm
 
 from pyod.models.cblof import CBLOF
 from pyod.models.cof import COF
+from pyod.models.pca import PCA as pyod_PCA
 from pyod.models.copod import COPOD
 from pyod.models.feature_bagging import FeatureBagging
 from pyod.models.hbos import HBOS
@@ -21,15 +22,15 @@ from sklearn.manifold import Isomap, MDS, TSNE, LocallyLinearEmbedding, Spectral
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.random_projection import GaussianRandomProjection
 
+from froid_od.classes.froid import FROID
 from froid_od.classes.wrappers.loop import FROID_LocalOutlierProbability
 
 
-class FROID():
+class LFROID(FROID):
 
     out_det_dict = {
         "knn": KNN,
         "lof": LocalOutlierFactor,
-        "loop": FROID_LocalOutlierProbability,
         "cof": COF,
         #"cblof": CBLOF,  # Il clustering non converge
         "ellenv": EllipticEnvelope,
@@ -40,46 +41,15 @@ class FROID():
         "isofor": IForest,
         "loda": LODA,
         "suod": SUOD,
-        "ocsvm": OCSVM,
+        "pca": pyod_PCA,
     }
 
     feat_red_dict = {
         "pca": PCA,
         "isomap": Isomap,
-        #"mds": MDS,
         "kpca": KernelPCA,
-        "tsne": TSNE,
+        #"tsne": TSNE,
         #"rsp": GaussianRandomProjection,
-        "lle": LocallyLinearEmbedding,
-        "se": SpectralEmbedding,
     }
     def __init__(self, out_det="all", feat_red="all", froid_iter=1, seed=42, n_jobs=1, **kwargs):
-        self.n_jobs = n_jobs
-        self.seed = seed
-        self.kwargs = kwargs
-        self.iter = froid_iter
-
-        self.fitted_ods = []
-        self.fitted_frs = []
-
-        self.kwargs["n_jobs"] = n_jobs
-        self.kwargs["seed"] = seed
-        self.kwargs["random_state"] = seed
-
-        if type(out_det) == str and out_det == "all":
-            self.out_det = FROID.out_det_dict
-        elif type(out_det) == dict:
-            self.out_det = out_det
-        elif type(out_det) == list:
-            self.out_det = dict([(k, v) for k, v in out_det.items() if k in FROID.out_det_dict])
-        else:
-            raise Exception(f"Unsupported type {type(out_det)} for out_det")
-
-        if type(feat_red) == str and feat_red == "all":
-            self.feat_red = FROID.feat_red_dict
-        elif type(feat_red) == dict:
-            self.feat_red = feat_red
-        elif type(feat_red) == list:
-            self.feat_red = dict([(k, v) for k, v in feat_red.items() if k in FROID.feat_red_dict])
-        else:
-            raise Exception(f"Unsupported type {type(out_det)} for out_det")
+        super().__init__(LFROID.out_det_dict, LFROID.feat_red_dict, froid_iter, seed, n_jobs, **kwargs)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from froid_od.classes.froid import FROID
+from froid_od.classes.lfroid import LFROID
 
 if __name__ == "__main__":
     df = pd.read_csv("datasets/bank.csv")
@@ -9,7 +10,9 @@ if __name__ == "__main__":
     X_train, X_test = train_test_split(df.values, test_size=.3, random_state=42)
 
     fr = FROID(seed=42, n_clusters=2)
-
     fr.fit(X_train)
-
     fr.transform(X_test)
+
+    lfr = LFROID(seed=42, n_clusters=2)
+    lfr.fit(X_train)
+    lfr.transform(X_test)
